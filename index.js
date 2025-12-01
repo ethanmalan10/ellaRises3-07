353071c4f1ca8bc0dfa3e2faaa25e0f510eeb8da
Isaac Fox <ikefox21@gmail.com>
2025-12-01 13:43:15 -0700

    new changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const layouts = require('express-ejs-layouts');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 
@@ -10,26 +10,31 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static files (CSS, client JS, images)
+// Static files (CSS, images, JS)
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ---------- View engine ----------
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(layouts);
-app.set('layout', 'partials/layout'); // views/partials/layout.ejs
+
+app.use(expressLayouts);
+
+// DEFAULT LAYOUT (DO NOT REMOVE)
+app.set('layout', 'partials/layout');  
+// This means every EJS view will automatically use views/partials/layout.ejs
 
 // ---------- Routes ----------
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => {
+  res.render('index');  // No layout() needed inside index.ejs
+});
 
-// Simple test pages you already created (optional)
-app.get('/login',        (req,res)=>res.render('login'));
-app.get('/participants', (req,res)=>res.render('participants'));
-app.get('/events',       (req,res)=>res.render('events'));
-app.get('/surveys',      (req,res)=>res.render('surveys'));
-app.get('/milestones',   (req,res)=>res.render('milestones'));
-app.get('/donations',    (req,res)=>res.render('donations'));
-app.get('/users',        (req,res)=>res.render('users'));
+app.get('/login',        (req, res) => res.render('login'));
+app.get('/participants', (req, res) => res.render('participants'));
+app.get('/events',       (req, res) => res.render('events'));
+app.get('/surveys',      (req, res) => res.render('surveys'));
+app.get('/milestones',   (req, res) => res.render('milestones'));
+app.get('/donations',    (req, res) => res.render('donations'));
+app.get('/users',        (req, res) => res.render('users'));
 
 // ---------- Start server ----------
 const PORT = process.env.PORT || 3000;
